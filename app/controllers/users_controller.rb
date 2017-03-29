1069f4ddae637d08d8db23574a600ae232855284
lgor89 <igor.penkovsky@gmail.com>
2017-03-29 21:54:21 +0300

    init shop model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create(user_params)
+    @user = User.create(user_params)
     redirect_to root_path
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
